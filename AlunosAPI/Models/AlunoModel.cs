9672a59f06fc70b43c3db5797c5fc431b2c03392
Anderson Prado <prado.app.dev@gmail.com>
2019-06-07 15:42:06 -0300

    melhoria da classe AlunoModel

--- AlunosAPI/Models/AlunoModel.cs
+++ AlunosAPI/Models/AlunoModel.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 namespace AlunosAPI.Models
 {
     public class AlunoModel
-    {        
+    {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
 
+        #region CRUD
+
+        //Create
         public void RegisterStudent()
         {
             DAL dalObj = new DAL();
@@ -20,5 +24,23 @@ namespace AlunosAPI.Models
                 "Values ({FirstName},{LastName},{Address},{Gender})";
             dalObj.ExecuteCommand(commandQuery);
         }
+
+        //Read
+        public void GetStudent()
+        {
+
+        }
+        //Update
+        public void UpdateStudent(int id)
+        {
+            Id = id;
+        }
+        //Delete
+        public void DeleteStudent(int id)
+        {
+            Id = id;
+        }
+
+        #endregion
     }
 }
