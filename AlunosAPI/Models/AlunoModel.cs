ab0f1112fc11fc1d94e55bf0428b14fb030ddb3b
Anderson Prado <prado.app.dev@gmail.com>
2019-06-07 15:37:11 -0300

    Criação do modelo de aluno e do método de registrar aluno no banco

---
+++ AlunosAPI/Models/AlunoModel.cs
@@ -0,0 +1,24 @@
+﻿using AlunosAPI.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlunosAPI.Models
+{
+    public class AlunoModel
+    {        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        public void RegisterStudent()
+        {
+            DAL dalObj = new DAL();
+            var commandQuery = $"insert into persons (firstname, lastname, address, gender)"+
+                "Values ({FirstName},{LastName},{Address},{Gender})";
+            dalObj.ExecuteCommand(commandQuery);
+        }
+    }
+}
